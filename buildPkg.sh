3a504946cd6ba3a41d7da16d1aa19b8d3afe0fb5
rajeevaswani010 <rajeevaswani010@gmail.com>
2023-06-14 17:48:39 +0400

    fixes

--- buildPkg.sh
+++ buildPkg.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 PKG=pkg_`date "+%H%M%S_%d%m%Y"`.zip
-zip -r ../${PKG} *
+zip -r -y ../pkg_`date "+%H%M%S_%d%m%Y"`.zip * -x CustomersImages\* BookimngImages\* VehicleImages\* public/CustomersImages public/VehicleImages public/BookimngImages
 echo "pkg $PKG created successfully."
