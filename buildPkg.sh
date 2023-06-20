ed8a70a42e41b2fabec5eef6e1b7bc2c3eed15f1
rajeevaswani010 <rajeevaswani010@gmail.com>
2023-06-21 00:33:01 +0400

    add more ignore files in buildPkg sh

--- buildPkg.sh
+++ buildPkg.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 PKG=pkg_`date "+%H%M%S_%d%m%Y"`.zip
-zip -r -y ../pkg_`date "+%H%M%S_%d%m%Y"`.zip * -x CustomersImages\* BookimngImages\* VehicleImages\* public/CustomersImages public/VehicleImages public/BookimngImages
+zip -r -y ../pkg_`date "+%H%M%S_%d%m%Y"`.zip * -x CustomersImages\* BookimngImages\* VehicleImages\* public/CustomersImages public/VehicleImages public/BookimngImages .env
 echo "pkg $PKG created successfully."
