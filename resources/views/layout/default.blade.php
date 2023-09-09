90dde92a20dbb9fe99c080bf403b846d46150731
rajeevaswani010 <rajeevaswani010@gmail.com>
2023-09-09 11:31:42 +0400

    bootstrap jquery slim and popper files added for UI

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -14,13 +14,16 @@
     <link rel="icon" href="https://nucleuz.app/public/favicon.png" type="image" sizes="16x16">
 
 
-    <!-- <link rel="stylesheet" href="{{ URL('public/newasserts/plugins/bootstrap/css/bootstrap.min.css') }}">
-    <script src="{{ URL('public/newasserts/plugins/bootstrap/js/bootstrap.min.js') }}"></script> -->
+    <link rel="stylesheet" href="{{ URL('public/newasserts/plugins/bootstrap/css/bootstrap.min.css') }}">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="{{ URL('public/newasserts/plugins/jquery/jquery-3.5.1.slim.min.js') }}"></script>
+    <script src="{{ URL('public/newasserts/plugins/popper/popper.min.js') }}"></script>
+    <script src="{{ URL('public/newasserts/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css"> -->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script> -->
 
     <!-- Favicon icon -->
 
